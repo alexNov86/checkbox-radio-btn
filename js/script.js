216cd67a81a7c892539c2f925f5c78f4618afe52
alexNovik <alexnovik86@gmail.com>
2022-07-12 21:40:39 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  // console.log(event);
-  const allCheckboxes = document.querySelectorAll(".checkbox");
-  console.log(allCheckboxes);
-  for (const checkbox of allCheckboxes) {
-    console.log(checkbox.getAttribute(checked));
-  }
-  // allCheckboxes.forEach((el) => {
-  //   console.log(el.checked);
-  // });
+  const allInputCheckboxes = document.querySelectorAll(
+    ".checkbox>[type='checkbox']"
+  );
+  allInputCheckboxes.forEach((el) => {
+    if (el.checked === true) el.parentElement.classList.add("active");
+    el.parentElement.addEventListener("click", (event) => {
+      if (el.checked === true) {
+        el.checked = false;
+      } else {
+        el.checked = true;
+      }
+      el.parentElement.classList.toggle("active");
+    });
+  });
 });
